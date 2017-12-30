75a2634c24d682e9ab1147f96a7c44309653c26a
krrishdholakia <krrishdholakia@gmail.com>
2017-12-30 08:49:49 +0530

    change structure of response messages

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ app.post('/webhook/', function (req, res) {
   	    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
       }
       if (event.postback) {
+		let text = event.message.text
 		if (text === 'Upcoming Events') {
 			sendTextMessage(sender, "https://www.facebook.com/events/1750621848580640/", token)
 			continue
