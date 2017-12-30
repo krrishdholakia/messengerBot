c98d102895bb78a07ca5cc57251af7f66926cbbb
krrishdholakia <krrishdholakia@gmail.com>
2017-12-30 09:38:39 +0530

    postback updates

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post('/webhook/', function (req, res) {
 	  }
 	  if (event.postback) {
 		let text = event.postback.title
-		if (text === 'Get Started') {
+		if (text === 'Upcoming Events') {
 			sendTextMessage(sender, "https://www.facebook.com/events/1750621848580640/")
 			continue
 		}
