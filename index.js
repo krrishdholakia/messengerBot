cf62e7cbe1d733e80ae88a72040d0d20e2be75d5
krrishdholakia <krrishdholakia@gmail.com>
2018-01-25 18:18:29 -0500

    automated page updates

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-
+const FB = require('fb');
+var fb = new FB.Facebook({version: 'v2.10', appId: '792651257580535', appSecret: '51bcf1c30a9487b1b54aa854d9118d20'});
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -30,6 +31,32 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
+FB.api('oauth/access_token', {
+    client_id: '792651257580535',
+    client_secret: '51bcf1c30a9487b1b54aa854d9118d20',
+    grant_type: 'client_credentials'
+}, function (res) {
+	console.log(res);
+    if(!res || res.error) {
+        console.log(!res ? 'error occurred' : res.error);
+        return;
+    }
+ 
+	var accessToken = res.access_token;
+	FB.api(
+	"/StartupExchange/events?time_filter=upcoming",
+	{
+		access_token: "792651257580535|pj83jueRZgWubbxWbpupmg5woKA"
+	},
+    function (response) {
+		console.log(response);
+      if (response && !response.error) {
+        /* handle the result */
+      }
+    }
+);
+});
+
 
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
@@ -39,6 +66,18 @@ app.post('/webhook/', function (req, res) {
 	  if (event.message && event.message.text) {
 		let text = event.message.text
 		if (text === 'Upcoming Events') {
+			FB.api(
+				"/StartupExchange/events?time_filter=upcoming",
+				{
+					access_token: "792651257580535|pj83jueRZgWubbxWbpupmg5woKA"
+				},
+				function (response) {
+					console.log(response);
+				  if (response && !response.error) {
+					/* handle the result */
+				  }
+				}
+			);
 			sendTextMessage(sender, "https://www.facebook.com/events/1750621848580640/")
 			continue
 		} else if (text == 'Get Started') {
@@ -118,42 +157,3 @@ function sendGenericMessage(sender) {
 	    }
     })
 }
-
-// function EventSignUp (sender) {
-//     let messageData = {
-// 	    "attachment": {
-// 		    "type": "template",
-// 		    "payload": {
-// 				"template_type": "generic",
-// 			    "elements": [{
-// 					"title": "Upcoming Event",
-// 				    "image_url": "https://pbs.twimg.com/profile_images/760579437519921152/xD0enLRb.jpg",
-// 				    "buttons": [{
-// 					    "type": "web_url",
-// 					    "url": "http://www.startup.exchange/resources.html",
-// 					    "title": "How to Start a Startup"
-// 				    }, {
-// 					    "type": "postback",
-// 					    "title": "Upcoming Events",
-// 					    "payload": "Payload for first element in a generic bubble",
-// 				    }],
-// 			    }]
-// 		    }
-// 	    }
-//     }
-//     request({
-// 	    url: 'https://graph.facebook.com/v2.6/me/messages',
-// 	    qs: {access_token:token},
-// 	    method: 'POST',
-// 	    json: {
-// 		    recipient: {id:sender},
-// 		    message: messageData,
-// 	    }
-//     }, function(error, response, body) {
-// 	    if (error) {
-// 		    console.log('Error sending messages: ', error)
-// 	    } else if (response.body.error) {
-// 		    console.log('Error: ', response.body.error)
-// 	    }
-//     })
-// }
